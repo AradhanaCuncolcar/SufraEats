ce36e365de165852712b4ed1feafeb3f4d991416
AradhanaCuncolcar <168082676+AradhanaCuncolcar@users.noreply.github.com>
2026-07-18 11:31:08 +0400

    Update sufraeats_app.py

--- sufraeats_app.py
+++ sufraeats_app.py
@@ -160,7 +160,7 @@ df_filtered = df_clean[
 # ==========================================
 if page == "📌 Expansion Strategy Mandate":
     st.title("🎯 Strategic Regional Expansion Recommendation")
-    st.markdown("<p style='font-size: 16px; color: #A0AEC0;'>Executive overview of geographic financial health, determining the optimal region for capital expansion based on actual realized profit rather than misleading top-line gross volumes.</p>", unsafe_allow_html=True)
+    #st.markdown("<p style='font-size: 16px; color: #A0AEC0;'>Executive overview of geographic financial health, determining the optimal region for capital expansion based on actual realized profit rather than misleading top-line gross volumes.</p>", unsafe_allow_html=True)
     
     st.markdown(f"""
     <div class="insight-box">
